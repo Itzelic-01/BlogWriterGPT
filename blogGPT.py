37581fd864c78f3070d1f99d6128f826ef4ba0a1
en3873 <en3873@gmail.com>
2023-10-25 20:36:13 +0900

    ver1_test3_231025

--- blogGPT.py
+++ blogGPT.py
@@ -3,63 +3,60 @@ import streamlit as st
 from streamlit_chat import message
 import time
 
+# OpenAPI의 API 키를 설정합니다.
 openai.api_key = st.secrets["MY_API_KEY"]
- 
+
+# GPT-4 모델을 사용하여 사용자의 프롬프트에 대한 응답을 생성하는 함수를 정의합니다.
 def generate_response(prompt):
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
             {
                 "role": "system",
-                "content": "You are an assistant who writes a safety technology blog.\nThe user is the person in charge of safety guidance on construction sites and specific facilities.\n\nWhen a user asks you to recommend a topic, please recommend a topic in a blog post to inform people about safety rules and safety-related laws as a safety guide.\n\nWhen a user asks you to write a blog post, the user writes a blog post in Korean to inform people about safety rules and safety-related laws.\n\nThe writing is written in honorific, not too serious, so that people can be interested.\nWrite sentences that can give people credibility using technical terms.\nIn the article, it should be composed of contents suitable for the purpose, such as type, purpose, cause, solution, and improvement, depending on the subject.\nDon't use strange sentences or words that aren't logical.\n\nIf the user provides the data in the following sentence, please answer based on the contents of the data and write it in the vicinity of 2500 characters in Korean based on all the information you can refer to:" + txt
+                "content": "I'm a safety guidance officer for construction sites and certain facilities.\n\n[Functions]\n1. Please recommend the 5 topic of the blog post when requested\n2. Please write a blog post when requested\n(Also, organize the table of contents professionally according to the topic and write post)\n3. Please change or increase your writing professionally\n\nIn honorifics, interesting, not serious\n\nIf the user provides the data next time, please refer to the schedule and fill it out:" + txt
             },
             {
                 "role": "user",
                 "content": prompt
             }
         ],
-        temperature=1,
+        temperature=0.8,
         max_tokens=3008,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
- 
+    # 생성된 응답을 반환합니다.
     message = response["choices"][0]["message"]["content"].replace("", "")
     return message
 
-# st.image()
+# 웹 애플리케이션의 헤더를 설정합니다.
 st.header("안전기술 블로그 글 생성")
 
-# Progress Bar
-progress_text = "Operation in progress. Please wait."
-my_bar = st.progress(0, text=progress_text)
-
-for percent_complete in range(100):
-    time.sleep(0.01)
-    my_bar.progress(percent_complete + 1, text=progress_text)
-time.sleep(1)
-my_bar.empty()
-
+# 웹 애플리케이션에 참고자료 입력 필드를 추가합니다.
 txt = st.text_area(
     "참고자료", placeholder="블로그 글 작성에 참고할 자료를 붙혀주세요 (선택사항)"
     )
 
+# 세션 상태를 초기화합니다.
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
+# 입력과 출력에 대한 폼을 생성합니다.
 with st.form('form', clear_on_submit=True):
     user_input = st.text_input('You: ', '', key='input')
     submitted = st.form_submit_button('Send')
 
+# 폼이 제출되면, 사용자의 입력에 대한 응답을 생성하고 세션 상태에 저장합니다.
 if submitted and user_input:
     output = generate_response(user_input)
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
 
+# 세션 상태에 저장된 모든 응답을 출력합니다.
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
